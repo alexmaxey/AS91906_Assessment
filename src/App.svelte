076af28427764c04ef112e6d139d8b4977f5ec2f
alexmaxey <60858622+alexmaxey@users.noreply.github.com>
2021-08-10 00:55:38 +0000

    Created questions list of all questions, three possible types (multichoice, word, number). Made output for multichoice questions. Made next and submit button so the user can submit their answer and see the result, then move on to the next question. Once all the questions have been done the overall points is displayed.

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,18 @@
 <script>
   let i = 0
+  let points = 0
+  let answering = true
     let questions = [
-    {question:"",
+    {question:"I am an odd number. Take away a letter and I become even. What am I?",
     type:"multi",
-    a1:"",
-    v1:"",
-    a2:"",
-    v2:"",
-    a3:"",
-    v3:"",
-    a4:"",
-    v4:"", 
+    a1:"3",
+    a2:"5",
+    a3:"7",
+    a4:"9",
     selection:"",
+    answer:"7",
     result:""},
-    
+
     {question:"",
     type:"word",
     answer:"",
@@ -26,39 +25,61 @@
     selection:"",
     result:""},
   ]
-
+function submit(){
+  answering = false
+  if (questions[i].selection == questions[i].answer){
+    questions[i].result = 'Correct'
+    points += 1
+  } else {
+    questions[i].result = 'Wrong'
+  }
+}
 function next(){
   i += 1
+  answering = true
 }
 </script>
 
 <h1>Riddles</h1>
-
-<button on:click={next}>
+{#if (i < questions.length)}
+{questions[i].question}
+<br>
+{#if (questions[i].type == 'multi')}
+<label>
+  <input type="radio" bind:group={questions[i].selection} value={questions[i].a1}>
+  {questions[i].a1}
+</label>
+<br>
+<label>
+  <input type="radio" bind:group={questions[i].selection} value={questions[i].a2}>
+  {questions[i].a2}
+</label>
+<br>
+<label>
+  <input type="radio" bind:group={questions[i].selection} value={questions[i].a3}>
+  {questions[i].a3}
+</label>
+<br>
+<label>
+  <input type="radio" bind:group={questions[i].selection} value={questions[i].a4}>
+  {questions[i].a4}
+</label>
+{/if}
+<br>
+{questions[i].result}
+<br>
+{#if answering}
+<button on:click={submit}>
   Submit
 </button>
+{:else}
+<button on:click={next}>
+  Next
+</button>
+{/if}
+{:else}
+You got {points} / {questions.length}
+{/if}
 
-{#each questions as queries}
-{queries.question}
-<br>
-<label>
-  <input type="radio" bind:group={queries.selection} value={queries.v1}>
-  {queries.a1}
-</label>
-<label>
-  <input type="radio" bind:group={queries.selection} value={queries.v2}>
-  {queries.a2}
-</label>
-<label>
-  <input type="radio" bind:group={queries.selection} value={queries.v3}>
-  {queries.a3}
-</label>
-<label>
-  <input type="radio" bind:group={queries.selection} value={queries.v4}>
-  {queries.a4}
-</label>
-<br>
-{queries.result}
-<br>
-{/each}
+
 
